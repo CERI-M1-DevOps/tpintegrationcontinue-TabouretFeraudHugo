0f2e6e4f3a944f856c9ce6dfee2e6f10399c0fb6
TabouretFeraudHugo <hugo.tabouret.feraud@gmail.com>
2025-09-18 17:02:00 +0200

    allez

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -223,14 +223,16 @@ public class ListeSimpleTest {
 
     @Test
     public void echanger2NoeudsSimilaires() {
-        listeATester.ajout(1);
+        listeATester.ajout(5);
+        listeATester.ajout(4);
         Noeud r1 = listeATester.tete;
-        listeATester.ajout(1);
+        listeATester.ajout(3);
+        listeATester.ajout(4);
         Noeud r2 = listeATester.tete;
-        assertEquals("ListeSimple(Noeud(1), Noeud(1))", listeATester.toString());
-        listeATester.echanger(r1,r2);
-        assertEquals("ListeSimple(Noeud(1),Noeud(1))", listeATester.toString());
-
+        listeATester.ajout(1);
+        assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(4), Noeud(5))",listeATester.toString() );
+        listeATester.echanger(r1, r2);
+        assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(4), Noeud(5))",listeATester.toString() );
     }
 
     @Test
